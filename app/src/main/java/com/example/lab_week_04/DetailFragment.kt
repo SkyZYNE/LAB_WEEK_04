c76f640d3c1351bd49164c3f1f1c00ef5b3ef2d0
SkyZYNE <raffaelgunawan19@gmail.com>
2025-09-20 02:59:14 +0700

    “Commit No. 3 – Tabbeb Navigation

--- app/src/main/java/com/example/lab_week_04/DetailFragment.kt
+++ app/src/main/java/com/example/lab_week_04/DetailFragment.kt
@@ -21,7 +21,6 @@ class DetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
